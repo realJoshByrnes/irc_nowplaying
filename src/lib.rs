5e6894504feb9023ff5833fbfd286d11faa5e65c
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-11-12 19:09:57 +1100

    Add debug_print dependency and refactor LOADINFO handling in mIRC integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,14 @@
 mod mirc;
 
-use mirc::{MircReturn, TimeoutReason, LOADINFO};
-use std::sync::atomic::{AtomicI32, AtomicPtr, Ordering};
-use std::sync::Mutex;
+use debug_print::debug_eprintln as deprintln;
+use mIRC::MircReturn;
+use mirc as mIRC;
+use std::sync::atomic::{AtomicI32, Ordering};
 use windows::core::{w, PCWSTR};
 use windows::Win32::Foundation::{BOOL, HWND};
-use windows::Win32::System::Threading::GetCurrentThreadId;
-use windows::Win32::UI::WindowsAndMessaging::{GetClassNameW, GetWindowThreadProcessId};
 
 // Global variables
-static M_CLIENT: Mutex<String> = Mutex::new(String::new());
-static M_VERSION: Mutex<u32> = Mutex::new(0);
-static M_MAX_BYTES: Mutex<u32> = Mutex::new(0);
-static M_HWND: AtomicPtr<HWND> = AtomicPtr::new(std::ptr::null_mut());
-
 static M_WAITING_FOR_MEDIA_CHANGE: AtomicI32 = AtomicI32::new(0);
-
-#[no_mangle]
-pub extern "stdcall" fn LoadDll(li: *mut LOADINFO) {
-    unsafe {
-        (*li).m_keep = BOOL(1); // Keep the DLL loaded
-        (*li).m_unicode = BOOL(1); // Use Unicode
-
-        // Store version in a global variable
-        let mut version = M_VERSION.lock().unwrap();
-        *version = (*li).m_version;
-
-        // Store the maximum bytes (for data & parms) in a global variable
-        let mut max_bytes = M_MAX_BYTES.lock().unwrap();
-        *max_bytes = (*li).m_bytes;
-
-        // Store the client name (mIRC/AdiIRC) in a global variable
-        let mut class_name: Vec<u16> = vec![0; 256];
-        let mut client = M_CLIENT.lock().unwrap();
-        if GetClassNameW((*li).m_hwnd, &mut class_name) > 0 {
-            let class_name = PCWSTR(class_name.as_ptr()).to_string().unwrap_or_default();
-            if class_name == "mIRC" {
-                // mIRC's main window class is "mIRC"
-                *client = "mIRC".to_string();
-            } else {
-                // Otherwise, assume AdiIRC
-                *client = "AdiIRC".to_string();
-            }
-        } else {
-            // If we can't get the class name, assume it's an unknown client
-            *client = "Unknown".to_string();
-        }
-
-        // Store the HWND in a global variable
-        M_HWND.store((*li).m_hwnd.0 as *mut HWND, Ordering::SeqCst);
-    }
-}
-
-#[no_mangle]
-pub extern "stdcall" fn UnloadDll(m_timeout: TimeoutReason) -> i32 {
-    match m_timeout {
-        TimeoutReason::Unload => 0,
-        TimeoutReason::Inactive => {
-            1 // Return return 0 to keep the DLL loaded, or 1 to allow it to be unloaded.
-        }
-        TimeoutReason::Exit => 0,
-    }
-}
-
-#[no_mangle]
-pub extern "stdcall" fn version(
-    _m_wnd: HWND,
-    _a_wnd: HWND,
-    data: PCWSTR,
-    _parms: PCWSTR,
-    _show: BOOL,
-    _nopause: BOOL,
-) -> MircReturn {
-    let name = env!("CARGO_PKG_NAME");
-    let version = env!("CARGO_PKG_VERSION");
-    let author = env!("CARGO_PKG_AUTHORS");
-    let arch = std::env::consts::ARCH;
-
-    let m_client = M_CLIENT.lock().unwrap().clone();
-
-    let m_version = *M_VERSION.lock().unwrap();
-    let m_version_low = m_version & 0xFFFF;
-    let m_version_high = m_version >> 16;
-
-    let input = format!(
-        "{} {} by {} on {} v{}.{} ({})\0",
-        name, version, author, m_client, m_version_low, m_version_high, arch
-    );
-    let wide_input: Vec<u16> = input.encode_utf16().collect();
-    let message = PCWSTR(wide_input.as_ptr());
-    unsafe {
-        std::ptr::copy_nonoverlapping(message.as_ptr(), data.0 as *mut u16, message.len() + 1);
-    }
-    MircReturn::Result // Return the result to mIRC
-}
-
-fn is_gui_thread() -> bool {
-    let hwnd = HWND(M_HWND.load(Ordering::SeqCst) as *mut _);
-    let thread_id = unsafe { GetWindowThreadProcessId(hwnd, None) };
-    let current_thread_id = unsafe { GetCurrentThreadId() };
-    thread_id == current_thread_id
-}
 
 #[no_mangle]
 pub extern "stdcall" fn wait_for_media(
@@ -111,10 +19,11 @@ pub extern "stdcall" fn wait_for_media(
     _show: BOOL,
     _nopause: BOOL,
 ) -> MircReturn {
-    if is_gui_thread() {
-        // Synchronous calls using $dll or /dll block the GUI thread, return immediately.
+    if !mIRC::is_dllcall() {
+        // Synchronous calls using $dll or /dll block the GUI thread, prevent this.
+        deprintln!("Prevented attempt to block GUI thread in wait_for_media()");
         let result =
-            w!("!echo -estgc info * The DLL routine you attempted touse is not allowed on the GUI thread, try using $ $+ dllcall() instead.");
+            w!("!echo -estgc info * The DLL routine you attempted to use is not allowed on the GUI thread, try using $ $+ dllcall() instead.");
         unsafe {
             std::ptr::copy_nonoverlapping(result.as_ptr(), data.0 as *mut u16, result.len() + 1);
         }
@@ -147,5 +56,5 @@ pub extern "stdcall" fn halt(
     unsafe {
         std::ptr::copy_nonoverlapping(result.as_ptr(), data.0 as *mut u16, result.len() + 1);
     }
-    MircReturn::Result // Return the result to mIRC
+    MircReturn::Command // Return the result to mIRC
 }
--- src/mirc.rs
+++ src/mirc.rs
@@ -1,14 +1,26 @@
+use debug_print::debug_eprintln as deprintln;
+use std::sync::Mutex;
+use windows::core::PCWSTR;
 use windows::Win32::Foundation::{BOOL, HWND};
+use windows::Win32::System::Threading::GetCurrentThreadId;
+use windows::Win32::UI::WindowsAndMessaging::GetClassNameW;
+use windows::Win32::UI::WindowsAndMessaging::GetWindowThreadProcessId;
 
 #[repr(C)]
+#[allow(non_snake_case)]
+#[derive(Clone)]
 pub struct LOADINFO {
-    pub m_version: u32,
-    pub m_hwnd: HWND,
-    pub m_keep: BOOL,
-    pub m_unicode: BOOL,
-    pub m_beta: u32,
-    pub m_bytes: u32,
+    pub mVersion: u32,
+    pub mHwnd: HWND,
+    pub mKeep: BOOL,
+    pub mUnicode: BOOL,
+    pub mBeta: u32,
+    pub mBytes: u32,
 }
+
+// Implement Send and Sync for LOADINFO
+unsafe impl Send for LOADINFO {}
+unsafe impl Sync for LOADINFO {}
 
 #[repr(i32)]
 pub enum MircReturn {
@@ -19,8 +31,109 @@ pub enum MircReturn {
 }
 
 #[repr(i32)]
+#[allow(dead_code)]
 pub enum TimeoutReason {
     Unload = 0,
     Inactive = 1,
     Exit = 2,
+}
+
+pub static M_LOADINFO: Mutex<LOADINFO> = Mutex::new(LOADINFO {
+    mVersion: 0,
+    mHwnd: HWND(std::ptr::null_mut()),
+    mKeep: BOOL(0),
+    mUnicode: BOOL(0),
+    mBeta: 0,
+    mBytes: 0,
+});
+
+#[no_mangle]
+pub extern "stdcall" fn LoadDll(li: *mut LOADINFO) {
+    deprintln!("Loading DLL");
+    unsafe {
+        deprintln!("Set mKeep = 1, mUnicode = 1");
+        (*li).mKeep = BOOL(1); // Keep the DLL loaded
+        (*li).mUnicode = BOOL(1); // Use Unicode
+
+        deprintln!("Store LOADINFO");
+        *M_LOADINFO.lock().unwrap() = (*li).clone();
+    }
+}
+
+#[no_mangle]
+pub extern "stdcall" fn UnloadDll(reason: TimeoutReason) -> i32 {
+    match reason {
+        TimeoutReason::Unload => 0,
+        TimeoutReason::Inactive => 1,
+        TimeoutReason::Exit => 0,
+    }
+}
+
+#[no_mangle]
+pub extern "stdcall" fn version(
+    _m_wnd: HWND,
+    _a_wnd: HWND,
+    data: PCWSTR,
+    _parms: PCWSTR,
+    _show: BOOL,
+    _nopause: BOOL,
+) -> MircReturn {
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    let author = env!("CARGO_PKG_AUTHORS");
+    let arch = std::env::consts::ARCH;
+
+    let m_client = get_client_name();
+    let loadinfo = M_LOADINFO.lock().unwrap();
+
+    let m_version = loadinfo.mVersion;
+    let m_version_low = m_version & 0xFFFF;
+    let m_version_high = m_version >> 16;
+
+    let input = format!(
+        // Format the string with the following variables
+        "{} {} by {} on {} v{}.{} ({})\0",
+        name, version, author, m_client, m_version_low, m_version_high, arch
+    );
+    let message: Vec<u16> = input.encode_utf16().collect(); // Convert to UTF-16
+    unsafe {
+        std::ptr::copy_nonoverlapping(message.as_ptr(), data.0 as *mut u16, message.len() + 1);
+    }
+    MircReturn::Result // Return the result to mIRC
+}
+
+struct ClientName;
+
+impl ClientName {
+    const MIRC: &'static str = "mIRC";
+    const ADIIRC: &'static str = "AdiIRC";
+    const UNKNOWN: &'static str = "Unknown";
+}
+
+pub fn get_client_name() -> String {
+    let hwnd = get_loadinfo().mHwnd;
+    let mut class_name: Vec<u16> = vec![0; 256];
+    unsafe {
+        if GetClassNameW(hwnd, &mut class_name) > 0 {
+            let class_name = PCWSTR(class_name.as_ptr()).to_string().unwrap_or_default(); // Convert to String
+            if class_name == ClientName::MIRC {
+                return class_name;
+            } else {
+                return ClientName::ADIIRC.to_string();
+            }
+        } else {
+            return ClientName::UNKNOWN.to_string();
+        }
+    }
+}
+
+pub fn get_loadinfo() -> LOADINFO {
+    M_LOADINFO.lock().unwrap().clone()
+}
+
+pub fn is_dllcall() -> bool {
+    let hwnd = get_loadinfo().mHwnd;
+    let thread_id = unsafe { GetWindowThreadProcessId(hwnd, None) };
+    let current_thread_id = unsafe { GetCurrentThreadId() };
+    thread_id != current_thread_id
 }
